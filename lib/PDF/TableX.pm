6a712226d6ccfadc6c1fbb2f309dadcbfbb6d2a5
Grzegorz Papkala <papkala@cpan.org>
2013-05-15 13:53:08 +0000

    Import of PAPKALA/PDF-TableX-0.012 from CPAN.
    gitpan-cpan-distribution: PDF-TableX
    gitpan-cpan-version:      0.012
    gitpan-cpan-path:         PAPKALA/PDF-TableX-0.012.tar.gz
    gitpan-cpan-author:       PAPKALA
    gitpan-cpan-maturity:     released

--- lib/PDF/TableX.pm
+++ lib/PDF/TableX.pm
@@ -13,7 +13,7 @@ use PDF::TableX::Cell;
 
 with 'PDF::TableX::Stylable';
 
-our $VERSION    = '0.011';
+our $VERSION    = '0.012';
 
 # public attrs
 has width         => (is => 'rw', isa => 'Num', default => 0);
@@ -184,11 +184,11 @@ sub cycle_background_color {
 
 =head1 NAME
 
-PDF::TableXMoose driven table generation module that is uses famous PDF::API2
+PDF::TableX - Moose driven table generation module that is uses famous PDF::API2
 
 =head1 VERSION
 
-Version 0.01
+Version 0.012
 
 
 =head1 SYNOPSIS
